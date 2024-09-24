058e3a3b0ee2109110d2af3a94b919454dbeb74f
dayayalaa <day1414@hotmail.com>
2024-09-24 18:24:27 -0300

    Se limpió las vistas, se enlazó la base de datos y está la vista de registro y el nav superior, hay que hacerle las condiciones

---
+++ clases/src/service/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAw0L5xNeHd-QRzNtQH0bmnPvdbddnyWSo",
+  authDomain: "clientesweb-58e8e.firebaseapp.com",
+  projectId: "clientesweb-58e8e",
+  storageBucket: "clientesweb-58e8e.appspot.com",
+  messagingSenderId: "965649110600",
+  appId: "1:965649110600:web:c580dbbe280dd773b2aa7d",
+  measurementId: "G-83HBM8PQFK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
